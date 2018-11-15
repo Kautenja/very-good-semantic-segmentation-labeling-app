6e411629c8bbe9746cbc6c9adf823f60bb6028dc
Kautenja <kautencreations@gmail.com>
2018-11-15 04:50:33 -0600

    update palette inputs

--- src/graphics/palette.py
+++ src/graphics/palette.py
@@ -1,6 +1,7 @@
 """A palette for working with semantic segmentation labeling."""
 import pandas as pd
 from appJar import gui
+from copy import deepcopy
 
 
 class Palette(object):
@@ -10,6 +11,28 @@ class Palette(object):
     WIDTH = 500
     # the height of the window
     HEIGHT = 700
+
+    DEFAULTS = {
+        'felzenszwalb': {
+            'scale': 100,
+            'sigma': 0.5,
+            'min_size': 50,
+        },
+        'slic': {
+            'n_segments': 250,
+            'compactness': 10,
+            'sigma': 1,
+        },
+        'quickshift': {
+            'kernel_size': 3,
+            'max_dist': 6,
+            'ratio': 0.5,
+        },
+        'watershed': {
+            'markers': 250,
+            'compactness': 0.001,
+        },
+    }
 
     def __init__(self, metadata: pd.DataFrame) -> None:
         """
@@ -23,27 +46,7 @@ class Palette(object):
 
         """
         self.metadata = metadata
-        self.segmentation_args = {
-            'felzenszwalb': {
-                'scale': 100,
-                'sigma': 0.5,
-                'min_size': 50,
-            },
-            'slic': {
-                'n_segments': 250,
-                'compactness': 10,
-                'sigma': 1,
-            },
-            'quickshift': {
-                'kernel_size': 3,
-                'max_dist': 6,
-                'ratio': 0.5,
-            },
-            'watershed': {
-                'markers': 250,
-                'compactness': 0.001,
-            },
-        }
+        self.segmentation_args = deepcopy(self.DEFAULTS)
         # create an application window
         dims = '{}x{}'.format(self.WIDTH, 10 * len(metadata) + self.HEIGHT)
         self._app = gui(self.__class__.__name__, dims)
@@ -86,19 +89,19 @@ class Palette(object):
 
         app.startTab("Felzenszwalb")
         app.startLabelFrame("Scale")
-        app.addLabelEntry("felzenszwalb_scale")
+        app.addValidationEntry("felzenszwalb_scale")
         app.stopLabelFrame()
         app.setEntryDefault('felzenszwalb_scale', self.segmentation_args['felzenszwalb']['scale'])
         app.setEntryChangeFunction("felzenszwalb_scale", self._did_change_felzenszwalb_scale)
 
         app.startLabelFrame("Sigma")
-        app.addLabelEntry("felzenszwalb_sigma")
+        app.addValidationEntry("felzenszwalb_sigma")
         app.stopLabelFrame()
         app.setEntryDefault('felzenszwalb_sigma', self.segmentation_args['felzenszwalb']['sigma'])
         app.setEntryChangeFunction("felzenszwalb_sigma", self._did_change_felzenszwalb_sigma)
 
         app.startLabelFrame("Minimum Size")
-        app.addLabelEntry("felzenszwalb_min_size")
+        app.addValidationEntry("felzenszwalb_min_size")
         app.stopLabelFrame()
         app.setEntryDefault('felzenszwalb_min_size', self.segmentation_args['felzenszwalb']['min_size'])
         app.setEntryChangeFunction("felzenszwalb_min_size", self._did_change_felzenszwalb_min_size)
@@ -108,19 +111,19 @@ class Palette(object):
 
         app.startTab("SLIC")
         app.startLabelFrame("Number of Segments")
-        app.addLabelEntry("slic_n_segments")
+        app.addValidationEntry("slic_n_segments")
         app.stopLabelFrame()
         app.setEntryDefault('slic_n_segments', self.segmentation_args['slic']['n_segments'])
         app.setEntryChangeFunction("slic_n_segments", self._did_change_slic_num_segments)
 
         app.startLabelFrame("Compactness ")
-        app.addLabelEntry("slic_compactness")
+        app.addValidationEntry("slic_compactness")
         app.stopLabelFrame()
         app.setEntryDefault('slic_compactness', self.segmentation_args['slic']['compactness'])
         app.setEntryChangeFunction("slic_compactness", self._did_change_slic_compactness)
 
         app.startLabelFrame("Sigma ")
-        app.addLabelEntry("slic_sigma")
+        app.addValidationEntry("slic_sigma")
         app.stopLabelFrame()
         app.setEntryDefault('slic_sigma', self.segmentation_args['slic']['sigma'])
         app.setEntryChangeFunction("slic_sigma", self._did_change_slic_sigma)
@@ -130,19 +133,19 @@ class Palette(object):
 
         app.startTab("Quickshift")
         app.startLabelFrame("Kernel Size")
-        app.addLabelEntry("quickshift_kernel_size")
+        app.addValidationEntry("quickshift_kernel_size")
         app.stopLabelFrame()
         app.setEntryDefault('quickshift_kernel_size', self.segmentation_args['quickshift']['kernel_size'])
         app.setEntryChangeFunction("quickshift_kernel_size", self._did_change_quickshift_kernel_size)
 
         app.startLabelFrame("Maximum Distance")
-        app.addLabelEntry("quickshift_max_dist")
+        app.addValidationEntry("quickshift_max_dist")
         app.stopLabelFrame()
         app.setEntryDefault('quickshift_max_dist', self.segmentation_args['quickshift']['max_dist'])
         app.setEntryChangeFunction("quickshift_max_dist", self._did_change_quickshift_max_distance)
 
         app.startLabelFrame("Ratio")
-        app.addLabelEntry("quickshift_ratio")
+        app.addValidationEntry("quickshift_ratio")
         app.stopLabelFrame()
         app.setEntryDefault('quickshift_ratio', self.segmentation_args['quickshift']['ratio'])
         app.setEntryChangeFunction("quickshift_ratio", self._did_change_quickshift_ratio)
@@ -152,13 +155,13 @@ class Palette(object):
 
         app.startTab("Watershed")
         app.startLabelFrame("Makers")
-        app.addLabelEntry("watershed_markers")
+        app.addValidationEntry("watershed_markers")
         app.stopLabelFrame()
         app.setEntryDefault('watershed_markers', self.segmentation_args['watershed']['markers'])
         app.setEntryChangeFunction("watershed_markers", self._did_change_watershed_markers)
 
         app.startLabelFrame("Compactness")
-        app.addLabelEntry("watershed_compactness")
+        app.addValidationEntry("watershed_compactness")
         app.stopLabelFrame()
         app.setEntryDefault('watershed_compactness', self.segmentation_args['watershed']['compactness'])
         app.setEntryChangeFunction("watershed_compactness", self._did_change_watershed_compactness)
@@ -186,59 +189,147 @@ class Palette(object):
         print(selected)
 
     def _did_change_felzenszwalb_scale(self, _):
-        selected = self._app.getEntry('felzenszwalb_scale')
-
-        self.segmentation_args['felzenszwalb']['scale'] = selected
+        if self._app.getEntry('felzenszwalb_scale') == "":
+            default = self.DEFAULTS['felzenszwalb']['scale']
+            self.segmentation_args['felzenszwalb']['scale'] = default
+            self._app.setEntryWaitingValidation('felzenszwalb_scale')
+            return
+        try:
+            selected = int(self._app.getEntry('felzenszwalb_scale'))
+            self.segmentation_args['felzenszwalb']['scale'] = selected
+            self._app.setEntryValid('felzenszwalb_scale')
+        except ValueError:
+            self._app.setEntryInvalid('felzenszwalb_scale')
 
     def _did_change_felzenszwalb_sigma(self, _):
-        selected = self._app.getEntry('felzenszwalb_sigma')
-
-        self.segmentation_args['felzenszwalb']['sigma'] = selected
+        if self._app.getEntry('felzenszwalb_sigma') == "":
+            default = self.DEFAULTS['felzenszwalb']['sigma']
+            self.segmentation_args['felzenszwalb']['sigma'] = default
+            self._app.setEntryWaitingValidation('felzenszwalb_sigma')
+            return
+        try:
+            selected = float(self._app.getEntry('felzenszwalb_sigma'))
+            self.segmentation_args['felzenszwalb']['sigma'] = selected
+            self._app.setEntryValid('felzenszwalb_sigma')
+        except ValueError:
+            self._app.setEntryInvalid('felzenszwalb_sigma')
 
     def _did_change_felzenszwalb_min_size(self, _):
-        selected = self._app.getEntry('felzenszwalb_min_size')
-
-        self.segmentation_args['felzenszwalb']['min_size'] = selected
+        if self._app.getEntry('felzenszwalb_min_size') == "":
+            default = self.DEFAULTS['felzenszwalb']['min_size']
+            self.segmentation_args['felzenszwalb']['min_size'] = default
+            self._app.setEntryWaitingValidation('felzenszwalb_min_size')
+            return
+        try:
+            selected = int(self._app.getEntry('felzenszwalb_min_size'))
+            self.segmentation_args['felzenszwalb']['min_size'] = selected
+            self._app.setEntryValid('felzenszwalb_min_size')
+        except ValueError:
+            self._app.setEntryInvalid('felzenszwalb_min_size')
 
     def _did_change_slic_num_segments(self, _):
-        selected = self._app.getEntry('slic_n_segments')
-
-        self.segmentation_args['slic']['n_segments'] = selected
+        if self._app.getEntry('slic_n_segments') == "":
+            default = self.DEFAULTS['slic']['n_segments']
+            self.segmentation_args['slic']['n_segments'] = default
+            self._app.setEntryWaitingValidation('slic_n_segments')
+            return
+        try:
+            selected = int(self._app.getEntry('slic_n_segments'))
+            self.segmentation_args['slic']['n_segments'] = selected
+            self._app.setEntryValid('slic_n_segments')
+        except ValueError:
+            self._app.setEntryInvalid('slic_n_segments')
 
     def _did_change_slic_compactness(self, _):
-        selected = self._app.getEntry('slic_compactness')
-
-        self.segmentation_args['slic']['compactness'] = selected
+        if self._app.getEntry('slic_compactness') == "":
+            default = self.DEFAULTS['slic']['compactness']
+            self.segmentation_args['slic']['compactness'] = default
+            self._app.setEntryWaitingValidation('slic_compactness')
+            return
+        try:
+            selected = float(self._app.getEntry('slic_compactness'))
+            self.segmentation_args['slic']['compactness'] = selected
+            self._app.setEntryValid('slic_compactness')
+        except ValueError:
+            self._app.setEntryInvalid('slic_compactness')
 
     def _did_change_slic_sigma(self, _):
-        selected = self._app.getEntry('slic_sigma')
-
-        self.segmentation_args['slic']['sigma'] = selected
+        if self._app.getEntry('slic_sigma') == "":
+            default = self.DEFAULTS['slic']['sigma']
+            self.segmentation_args['slic']['sigma'] = default
+            self._app.setEntryWaitingValidation('slic_sigma')
+            return
+        try:
+            selected = float(self._app.getEntry('slic_sigma'))
+            self.segmentation_args['slic']['sigma'] = selected
+            self._app.setEntryValid('slic_sigma')
+        except ValueError:
+            self._app.setEntryInvalid('slic_sigma')
 
     def _did_change_quickshift_kernel_size(self, _):
-        selected = self._app.getEntry('quickshift_kernel_size')
-
-        self.segmentation_args['quickshift']['kernel_size'] = selected
+        if self._app.getEntry('quickshift_kernel_size') == "":
+            default = self.DEFAULTS['quickshift']['kernel_size']
+            self.segmentation_args['quickshift']['kernel_size'] = default
+            self._app.setEntryWaitingValidation('quickshift_kernel_size')
+            return
+        try:
+            selected = int(self._app.getEntry('quickshift_kernel_size'))
+            self.segmentation_args['quickshift']['kernel_size'] = selected
+            self._app.setEntryValid('quickshift_kernel_size')
+        except ValueError:
+            self._app.setEntryInvalid('quickshift_kernel_size')
 
     def _did_change_quickshift_max_distance(self, _):
-        selected = self._app.getEntry('quickshift_max_dist')
-
-        self.segmentation_args['quickshift']['max_dist'] = selected
+        if self._app.getEntry('quickshift_max_dist') == "":
+            default = self.DEFAULTS['quickshift']['max_dist']
+            self.segmentation_args['quickshift']['max_dist'] = default
+            self._app.setEntryWaitingValidation('quickshift_max_dist')
+            return
+        try:
+            selected = int(self._app.getEntry('quickshift_max_dist'))
+            self.segmentation_args['quickshift']['max_dist'] = selected
+            self._app.setEntryValid('quickshift_max_dist')
+        except ValueError:
+            self._app.setEntryInvalid('quickshift_max_dist')
 
     def _did_change_quickshift_ratio(self, _):
-        selected = self._app.getEntry('quickshift_ratio')
-
-        self.segmentation_args['quickshift']['ratio'] = selected
+        if self._app.getEntry('quickshift_ratio') == "":
+            default = self.DEFAULTS['quickshift']['ratio']
+            self.segmentation_args['quickshift']['ratio'] = default
+            self._app.setEntryWaitingValidation('quickshift_ratio')
+            return
+        try:
+            selected = float(self._app.getEntry('quickshift_ratio'))
+            self.segmentation_args['quickshift']['ratio'] = selected
+            self._app.setEntryValid('quickshift_ratio')
+        except ValueError:
+            self._app.setEntryInvalid('quickshift_ratio')
 
     def _did_change_watershed_markers(self, _):
-        selected = self._app.getEntry('watershed_markers')
-
-        self.segmentation_args['watershed']['markers'] = selected
+        if self._app.getEntry('watershed_markers') == "":
+            default = self.DEFAULTS['watershed']['markers']
+            self.segmentation_args['watershed']['markers'] = default
+            self._app.setEntryWaitingValidation('watershed_markers')
+            return
+        try:
+            selected = int(self._app.getEntry('watershed_markers'))
+            self.segmentation_args['watershed']['markers'] = selected
+            self._app.setEntryValid('watershed_markers')
+        except ValueError:
+            self._app.setEntryInvalid('watershed_markers')
 
     def _did_change_watershed_compactness(self, _):
-        selected = self._app.getEntry('watershed_compactness')
-
-        self.segmentation_args['watershed']['compactness'] = selected
+        if self._app.getEntry('watershed_compactness') == "":
+            default = self.DEFAULTS['watershed']['compactness']
+            self.segmentation_args['watershed']['compactness'] = default
+            self._app.setEntryWaitingValidation('watershed_compactness')
+            return
+        try:
+            selected = float(self._app.getEntry('watershed_compactness'))
+            self.segmentation_args['watershed']['compactness'] = selected
+            self._app.setEntryValid('watershed_compactness')
+        except ValueError:
+            self._app.setEntryInvalid('watershed_compactness')
 
     def _did_change_label(self, _):
         selected = self._app.getListBox('labels')[0]
