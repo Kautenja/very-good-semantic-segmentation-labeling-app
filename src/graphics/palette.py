ec08d25c1100adb2e5384ce3d6d0c144209bab39
Kautenja <kautencreations@gmail.com>
2018-11-15 05:02:07 -0600

    select defaults

--- src/graphics/palette.py
+++ src/graphics/palette.py
@@ -11,7 +11,9 @@ class Palette(object):
     WIDTH = 500
     # the height of the window
     HEIGHT = 700
-
+    # the height per label entry
+    LABEL_HEIGHT = 10
+    # the default arguments for the view controller
     DEFAULTS = {
         'felzenszwalb': {
             'scale': 100,
@@ -48,147 +50,147 @@ class Palette(object):
         self.metadata = metadata
         self.segmentation_args = deepcopy(self.DEFAULTS)
         # create an application window
-        dims = '{}x{}'.format(self.WIDTH, 10 * len(metadata) + self.HEIGHT)
+        height = self.LABEL_HEIGHT * len(metadata) + self.HEIGHT
+        dims = '{}x{}'.format(self.WIDTH, height)
         self._app = gui(self.__class__.__name__, dims)
         self._window_did_load(self._app)
         self._view_did_load(self._app)
 
-
     # MARK: View Hierarchy
 
-    def _window_did_load(self, app):
+    def _window_did_load(self, app) -> None:
         """Perform global window decoration."""
         app.setFont(14)
 
-    def _view_did_load(self, app):
+    def _view_did_load(self, app) -> None:
         """Setup the sub-views after the view is loaded into memory."""
-
+        # setup the paint style
         app.startLabelFrame("Paint Style")
         app.addRadioButton("paint", "Brush")
         app.addRadioButton("paint", "Super Pixel")
         app.setRadioButtonChangeFunction('paint', self._did_change_paint)
+        app.setRadioButton('paint', 'Brush')
         app.stopLabelFrame()
-
+        # setup the brush size slider
         app.addLabelScale('Brush Size')
-        app.setScaleRange('Brush Size', 0, 50, curr=7)
+        app.setScaleRange('Brush Size', 0, 50)
         app.showScaleIntervals('Brush Size', 5)
         app.showScaleValue('Brush Size', show=True)
         app.setScaleChangeFunction('Brush Size', self._did_change_brush_size)
-
+        app.setScale('Brush Size', 5)
+        # setup the super pixel algorithm picker
         app.startLabelFrame("Super Pixel Algorithm")
         app.addRadioButton("super_pixel", "Felzenszwalb")
         app.addRadioButton("super_pixel", "SLIC")
         app.addRadioButton("super_pixel", "Quickshift")
         app.addRadioButton("super_pixel", "Watershed")
         app.setRadioButtonChangeFunction('super_pixel', self._did_change_super_pixel)
+        app.setRadioButton('super_pixel', 'Felzenszwalb')
         app.stopLabelFrame()
-
-
-
+        # super pixel algorithm parameters
         app.startTabbedFrame("SuperPixel")
-
+        # Felzenszwalb
+        #     Scale
         app.startTab("Felzenszwalb")
         app.startLabelFrame("Scale")
         app.addValidationEntry("felzenszwalb_scale")
         app.stopLabelFrame()
         app.setEntryDefault('felzenszwalb_scale', self.segmentation_args['felzenszwalb']['scale'])
         app.setEntryChangeFunction("felzenszwalb_scale", self._did_change_felzenszwalb_scale)
-
+        #     Sigma
         app.startLabelFrame("Sigma")
         app.addValidationEntry("felzenszwalb_sigma")
         app.stopLabelFrame()
         app.setEntryDefault('felzenszwalb_sigma', self.segmentation_args['felzenszwalb']['sigma'])
         app.setEntryChangeFunction("felzenszwalb_sigma", self._did_change_felzenszwalb_sigma)
-
+        #     Minimum Size
         app.startLabelFrame("Minimum Size")
         app.addValidationEntry("felzenszwalb_min_size")
         app.stopLabelFrame()
         app.setEntryDefault('felzenszwalb_min_size', self.segmentation_args['felzenszwalb']['min_size'])
         app.setEntryChangeFunction("felzenszwalb_min_size", self._did_change_felzenszwalb_min_size)
         app.stopTab()
-
-
-
+        # SLIC
+        #     Number of Segments
         app.startTab("SLIC")
         app.startLabelFrame("Number of Segments")
         app.addValidationEntry("slic_n_segments")
         app.stopLabelFrame()
         app.setEntryDefault('slic_n_segments', self.segmentation_args['slic']['n_segments'])
         app.setEntryChangeFunction("slic_n_segments", self._did_change_slic_num_segments)
-
+        #     Compactness
         app.startLabelFrame("Compactness ")
         app.addValidationEntry("slic_compactness")
         app.stopLabelFrame()
         app.setEntryDefault('slic_compactness', self.segmentation_args['slic']['compactness'])
         app.setEntryChangeFunction("slic_compactness", self._did_change_slic_compactness)
-
+        #     Sigma
         app.startLabelFrame("Sigma ")
         app.addValidationEntry("slic_sigma")
         app.stopLabelFrame()
         app.setEntryDefault('slic_sigma', self.segmentation_args['slic']['sigma'])
         app.setEntryChangeFunction("slic_sigma", self._did_change_slic_sigma)
         app.stopTab()
-
-
-
+        # Quickshift
+        #     Kernel Size
         app.startTab("Quickshift")
         app.startLabelFrame("Kernel Size")
         app.addValidationEntry("quickshift_kernel_size")
         app.stopLabelFrame()
         app.setEntryDefault('quickshift_kernel_size', self.segmentation_args['quickshift']['kernel_size'])
         app.setEntryChangeFunction("quickshift_kernel_size", self._did_change_quickshift_kernel_size)
-
+        #     Maximum Distance
         app.startLabelFrame("Maximum Distance")
         app.addValidationEntry("quickshift_max_dist")
         app.stopLabelFrame()
         app.setEntryDefault('quickshift_max_dist', self.segmentation_args['quickshift']['max_dist'])
         app.setEntryChangeFunction("quickshift_max_dist", self._did_change_quickshift_max_distance)
-
+        #     Ratio
         app.startLabelFrame("Ratio")
         app.addValidationEntry("quickshift_ratio")
         app.stopLabelFrame()
         app.setEntryDefault('quickshift_ratio', self.segmentation_args['quickshift']['ratio'])
         app.setEntryChangeFunction("quickshift_ratio", self._did_change_quickshift_ratio)
         app.stopTab()
-
-
-
-        app.startTab("Watershed")
-        app.startLabelFrame("Makers")
-        app.addValidationEntry("watershed_markers")
+        # Watershed
+        #     Markers
+        app.startTab('Watershed')
+        app.startLabelFrame('Makers')
+        app.addValidationEntry('watershed_markers')
         app.stopLabelFrame()
         app.setEntryDefault('watershed_markers', self.segmentation_args['watershed']['markers'])
-        app.setEntryChangeFunction("watershed_markers", self._did_change_watershed_markers)
-
-        app.startLabelFrame("Compactness")
-        app.addValidationEntry("watershed_compactness")
+        app.setEntryChangeFunction('watershed_markers', self._did_change_watershed_markers)
+        #     Compactness
+        app.startLabelFrame('Compactness')
+        app.addValidationEntry('watershed_compactness')
         app.stopLabelFrame()
         app.setEntryDefault('watershed_compactness', self.segmentation_args['watershed']['compactness'])
-        app.setEntryChangeFunction("watershed_compactness", self._did_change_watershed_compactness)
+        app.setEntryChangeFunction('watershed_compactness', self._did_change_watershed_compactness)
         app.stopTab()
         app.stopTabbedFrame()
-
+        # add a list box for selecting the labels
         app.addListBox('labels', self.metadata['label'])
         for idx, color in enumerate(self.metadata['rgb']):
             app.setListItemAtPosBg('labels', idx, '#%02x%02x%02x' % color)
             app.setListItemAtPosFg('labels', idx, '#FFFFFF')
         app.setListBoxChangeFunction('labels', self._did_change_label)
+        app.selectListItem('labels', self.metadata['label'][0])
 
     # MARK: Callbacks
 
-    def _did_change_paint(self, _):
+    def _did_change_paint(self, _) -> None:
         selected = self._app.getRadioButton('paint')
         print(selected)
 
-    def _did_change_brush_size(self, _):
+    def _did_change_brush_size(self, _) -> None:
         selected = self._app.getScale('Brush Size')
         print(selected)
 
-    def _did_change_super_pixel(self, _):
+    def _did_change_super_pixel(self, _) -> None:
         selected = self._app.getRadioButton('super_pixel')
         print(selected)
 
-    def _did_change_felzenszwalb_scale(self, _):
+    def _did_change_felzenszwalb_scale(self, _) -> None:
         if self._app.getEntry('felzenszwalb_scale') == "":
             default = self.DEFAULTS['felzenszwalb']['scale']
             self.segmentation_args['felzenszwalb']['scale'] = default
@@ -201,7 +203,7 @@ class Palette(object):
         except ValueError:
             self._app.setEntryInvalid('felzenszwalb_scale')
 
-    def _did_change_felzenszwalb_sigma(self, _):
+    def _did_change_felzenszwalb_sigma(self, _) -> None:
         if self._app.getEntry('felzenszwalb_sigma') == "":
             default = self.DEFAULTS['felzenszwalb']['sigma']
             self.segmentation_args['felzenszwalb']['sigma'] = default
@@ -214,7 +216,7 @@ class Palette(object):
         except ValueError:
             self._app.setEntryInvalid('felzenszwalb_sigma')
 
-    def _did_change_felzenszwalb_min_size(self, _):
+    def _did_change_felzenszwalb_min_size(self, _) -> None:
         if self._app.getEntry('felzenszwalb_min_size') == "":
             default = self.DEFAULTS['felzenszwalb']['min_size']
             self.segmentation_args['felzenszwalb']['min_size'] = default
@@ -227,7 +229,7 @@ class Palette(object):
         except ValueError:
             self._app.setEntryInvalid('felzenszwalb_min_size')
 
-    def _did_change_slic_num_segments(self, _):
+    def _did_change_slic_num_segments(self, _) -> None:
         if self._app.getEntry('slic_n_segments') == "":
             default = self.DEFAULTS['slic']['n_segments']
             self.segmentation_args['slic']['n_segments'] = default
@@ -240,7 +242,7 @@ class Palette(object):
         except ValueError:
             self._app.setEntryInvalid('slic_n_segments')
 
-    def _did_change_slic_compactness(self, _):
+    def _did_change_slic_compactness(self, _) -> None:
         if self._app.getEntry('slic_compactness') == "":
             default = self.DEFAULTS['slic']['compactness']
             self.segmentation_args['slic']['compactness'] = default
@@ -253,7 +255,7 @@ class Palette(object):
         except ValueError:
             self._app.setEntryInvalid('slic_compactness')
 
-    def _did_change_slic_sigma(self, _):
+    def _did_change_slic_sigma(self, _) -> None:
         if self._app.getEntry('slic_sigma') == "":
             default = self.DEFAULTS['slic']['sigma']
             self.segmentation_args['slic']['sigma'] = default
@@ -266,7 +268,7 @@ class Palette(object):
         except ValueError:
             self._app.setEntryInvalid('slic_sigma')
 
-    def _did_change_quickshift_kernel_size(self, _):
+    def _did_change_quickshift_kernel_size(self, _) -> None:
         if self._app.getEntry('quickshift_kernel_size') == "":
             default = self.DEFAULTS['quickshift']['kernel_size']
             self.segmentation_args['quickshift']['kernel_size'] = default
@@ -279,7 +281,7 @@ class Palette(object):
         except ValueError:
             self._app.setEntryInvalid('quickshift_kernel_size')
 
-    def _did_change_quickshift_max_distance(self, _):
+    def _did_change_quickshift_max_distance(self, _) -> None:
         if self._app.getEntry('quickshift_max_dist') == "":
             default = self.DEFAULTS['quickshift']['max_dist']
             self.segmentation_args['quickshift']['max_dist'] = default
@@ -292,7 +294,7 @@ class Palette(object):
         except ValueError:
             self._app.setEntryInvalid('quickshift_max_dist')
 
-    def _did_change_quickshift_ratio(self, _):
+    def _did_change_quickshift_ratio(self, _) -> None:
         if self._app.getEntry('quickshift_ratio') == "":
             default = self.DEFAULTS['quickshift']['ratio']
             self.segmentation_args['quickshift']['ratio'] = default
@@ -305,7 +307,7 @@ class Palette(object):
         except ValueError:
             self._app.setEntryInvalid('quickshift_ratio')
 
-    def _did_change_watershed_markers(self, _):
+    def _did_change_watershed_markers(self, _) -> None:
         if self._app.getEntry('watershed_markers') == "":
             default = self.DEFAULTS['watershed']['markers']
             self.segmentation_args['watershed']['markers'] = default
@@ -318,7 +320,7 @@ class Palette(object):
         except ValueError:
             self._app.setEntryInvalid('watershed_markers')
 
-    def _did_change_watershed_compactness(self, _):
+    def _did_change_watershed_compactness(self, _) -> None:
         if self._app.getEntry('watershed_compactness') == "":
             default = self.DEFAULTS['watershed']['compactness']
             self.segmentation_args['watershed']['compactness'] = default
@@ -331,12 +333,12 @@ class Palette(object):
         except ValueError:
             self._app.setEntryInvalid('watershed_compactness')
 
-    def _did_change_label(self, _):
+    def _did_change_label(self, _) -> None:
         selected = self._app.getListBox('labels')[0]
         print(selected)
 
     # MARK: Execution Stack
 
-    def run(self):
+    def run(self) -> None:
         """Start the application."""
         self._app.go()
