c10f1206ca5dd9d4583d39300cb6017c61286db9
Kautenja <kautencreations@gmail.com>
2018-11-15 01:02:35 -0600

    logic to load metadat

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
 """The main entry point for the data labeler."""
 import argparse
+import ast
 import numpy as np
+import pandas as pd
 from PIL import Image
 from src.data_labeler import DataLabeler
 
@@ -10,13 +12,19 @@ PARSER = argparse.ArgumentParser(description=__doc__)
 # add an argument for the image to segment
 PARSER.add_argument('--image', '-i',
     type=str,
-    help='TODO.',
+    help='the input image to segment.',
+    required=True,
+)
+# add an argument for the labeling metadata
+PARSER.add_argument('--metadata', '-m',
+    type=str,
+    help='the labeling metadata as a .csv file.',
     required=True,
 )
 # add an argument for the an existing segmentation to start with
 PARSER.add_argument('--segmentation', '-s',
     type=str,
-    help='TODO.',
+    help='the a priori segmentation if there is one.',
     required=False,
     default=None,
 )
@@ -26,14 +34,23 @@ PARSER.add_argument('--segmentation', '-s',
 ARGS = PARSER.parse_args()
 
 
+# load the input image to segment
 with Image.open(ARGS.image) as image_file:
     ARGS.image = np.array(image_file)
 
 
+# load the metadata
+ARGS.metadata = pd.read_csv(ARGS.metadata)
+ARGS.metadata['rgb'] = ARGS.metadata['rgb'].apply(ast.literal_eval)
+
+
+# load the a priori segmentation if there is one
 if ARGS.segmentation is not None:
     with Image.open(ARGS.segmentation) as segmentation_file:
         ARGS.segmentation = np.array(segmentation_file)
 
 
-LABELER = DataLabeler(ARGS.image, ARGS.segmentation)
+# create the data labeler application
+LABELER = DataLabeler(ARGS.image, ARGS.metadata, ARGS.segmentation)
+# run the data labeler application
 LABELER.run()
