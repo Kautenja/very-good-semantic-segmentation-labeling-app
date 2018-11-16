7be9bc739df7ff899ac2400190a3a1c47997b3d4
Kautenja <kautencreations@gmail.com>
2018-11-15 22:02:06 -0600

    update cursor

--- src/cursor.py
+++ src/cursor.py
@@ -21,18 +21,46 @@ def make_circle(radius: int, dtype: str='uint8') -> np.ndarray:
     box = np.zeros((length, length), dtype=dtype)
     # get the coordinates for the circle
     x_pixels, y_pixels = draw_circle(radius, radius, radius)
-    # set the coordinates of the circle in the box to the given color
+    # set the coordinates of the circle in the box
     box[x_pixels, y_pixels] = 1
 
     return box
 
 
-def make_cursor(radius: int, color: tuple=(255, 255, 255)) -> np.ndarray:
+def make_ring(inner_radius: int, outer_radius: int, dtype: str='uint8') -> np.ndarray:
+    """
+    Make a circle with a given radius.
+
+    Args:
+        inner_radius: the inner radius of the circle to draw
+        outer_radius: the outer radius of the circle to draw
+
+    Returns:
+        a NumPy matrix of size 2 * radius + 1 with a circle in it.
+
+    """
+    # if the radius is even, make it odd
+    length = 2 * outer_radius + 1
+    # create an RGBA box to house the circle in
+    box = np.zeros((length, length), dtype=dtype)
+    # get the coordinates for the outer circle
+    x_pixels, y_pixels = draw_circle(outer_radius, outer_radius, outer_radius)
+    # set the coordinates of the circle in the box
+    box[x_pixels, y_pixels] = 1
+    # get the coordinates for the inner circle
+    x_pixels, y_pixels = draw_circle(outer_radius, outer_radius, inner_radius)
+    # set the coordinates of the circle in the box
+    box[x_pixels, y_pixels] = 0
+
+    return box
+
+
+def make_cursor(circle: np.ndarray, color: tuple=(255, 255, 255)) -> np.ndarray:
     """
     Make an RGBA cursor with a given radius and color.
 
     Args:
-        radius: the radius of the circle cursor to make
+        circle: the matrix with the circle to make a cursor image from
         color: the color of the circle to make
 
     Returns:
@@ -40,7 +68,7 @@ def make_cursor(radius: int, color: tuple=(255, 255, 255)) -> np.ndarray:
 
     """
     # create a matrix with a circle in it
-    circle = make_circle(radius)[..., None]
+    circle = circle[..., None]
     # create a circle in a matrix and assign the color
     circle_image = np.concatenate(3 * [circle], axis=-1) * color
     # add an alpha channel
@@ -66,7 +94,8 @@ def pyglet_cursor(image: np.ndarray) -> pyglet.window.ImageMouseCursor:
         pitch=image.shape[1] * -len('RGBA')
     )
     # return the image mouse cursor
-    return pyglet.window.ImageMouseCursor(image_data)
+    hot_shape = int(image.shape[0] / 2), int(image.shape[1] / 2)
+    return pyglet.window.ImageMouseCursor(image_data, *hot_shape)
 
 
 # explicitly define the outward facing API of this module
--- src/tests/test_cursor.py
+++ src/tests/test_cursor.py
@@ -2,7 +2,7 @@
 from unittest import TestCase
 import numpy as np
 from ..cursor import make_circle
-from ..cursor import make_cursor
+from ..cursor import make_cursor, make_ring
 from ..cursor import pyglet_cursor
 
 
@@ -30,6 +30,19 @@ class ShouldCreateCircleWithRadius2(TestCase):
         self.assertTrue(np.array_equal(expected, circle))
 
 
+class ShouldCreateRingWithRadius2(TestCase):
+    def test(self):
+        circle = make_ring(1, 2)
+        expected = np.array([
+            [0, 0, 0, 0, 0,],
+            [0, 1, 1, 1, 0,],
+            [0, 1, 0, 1, 0,],
+            [0, 1, 1, 1, 0,],
+            [0, 0, 0, 0, 0,],
+        ])
+        self.assertTrue(np.array_equal(expected, circle))
+
+
 class ShouldCreateCircleWithRadius3(TestCase):
     def test(self):
         circle = make_circle(3)
@@ -39,6 +52,21 @@ class ShouldCreateCircleWithRadius3(TestCase):
             [0, 1, 1, 1, 1, 1, 0,],
             [0, 1, 1, 1, 1, 1, 0,],
             [0, 1, 1, 1, 1, 1, 0,],
+            [0, 1, 1, 1, 1, 1, 0,],
+            [0, 0, 0, 0, 0, 0, 0,],
+        ])
+        self.assertTrue(np.array_equal(expected, circle))
+
+
+class ShouldCreateRingWithRadius3(TestCase):
+    def test(self):
+        circle = make_ring(2, 3)
+        expected = np.array([
+            [0, 0, 0, 0, 0, 0, 0,],
+            [0, 1, 1, 1, 1, 1, 0,],
+            [0, 1, 0, 0, 0, 1, 0,],
+            [0, 1, 0, 0, 0, 1, 0,],
+            [0, 1, 0, 0, 0, 1, 0,],
             [0, 1, 1, 1, 1, 1, 0,],
             [0, 0, 0, 0, 0, 0, 0,],
         ])
@@ -74,9 +102,39 @@ class ShouldCreateCircleWithRadius10(TestCase):
         self.assertTrue(np.array_equal(expected, circle))
 
 
+class ShouldCreateRingWithRadius10(TestCase):
+    def test(self):
+        circle = make_ring(9, 10)
+        expected = np.array([
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+            [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,],
+            [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0,],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,],
+            [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0,],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,],
+            [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+        ])
+        self.assertTrue(np.array_equal(expected, circle))
+
+
 class ShouldCreateCursorWithRadius1(TestCase):
     def test(self):
-        circle = make_cursor(1, color=(33, 66, 99))
+        circle = make_circle(1)
+        circle = make_cursor(circle, color=(33, 66, 99))
         expected = np.array([
             [
                 [  0,   0,   0,   0,],
@@ -97,7 +155,8 @@ class ShouldCreateCursorWithRadius1(TestCase):
 
 class ShouldCreateCursorWithRadius2(TestCase):
     def test(self):
-        circle = make_cursor(2, color=(33, 66, 99))
+        circle = make_circle(2)
+        circle = make_cursor(circle, color=(33, 66, 99))
         expected = np.array([
             [
                 [  0,   0,   0,   0,],
@@ -136,7 +195,8 @@ class ShouldCreateCursorWithRadius2(TestCase):
 
 class ShouldCreateCursorWithRadius3(TestCase):
     def test(self):
-        circle = make_cursor(3, color=(33, 66, 99))
+        circle = make_circle(3)
+        circle = make_cursor(circle, color=(33, 66, 99))
         expected = np.array([
             [
                 [  0,   0,   0,   0,],
@@ -201,5 +261,6 @@ class ShouldCreateCursorWithRadius3(TestCase):
 
 class ShouldMakePygletCursor(TestCase):
     def test(self):
-        cursor = make_cursor(3, color=(33, 66, 99))
+        circle = make_circle(3)
+        cursor = make_cursor(circle, color=(33, 66, 99))
         mouse_cursor = pyglet_cursor(cursor)
