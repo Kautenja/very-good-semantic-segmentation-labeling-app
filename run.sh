f5a98557211a09132adcf5124f9218af8f715061
Kautenja <kautencreations@gmail.com>
2018-11-22 08:46:01 -0600

    run script

---
+++ run.sh
@@ -0,0 +1,5 @@
+python3 . \
+  -i /media/bitcommander/SSD1/Projects/perception-augmentation-in-autonomous-vehicles-data-collection/images/4/X/data/$1.png \
+  -s /media/bitcommander/SSD1/Projects/perception-augmentation-in-autonomous-vehicles-data-collection/images/4/y/data/$1.png \
+  -m dummy/metadata.csv
+
